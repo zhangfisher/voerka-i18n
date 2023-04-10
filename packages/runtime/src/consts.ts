ffbf5ad9ad12bb4432382f174688224ad1180e5a
wxzhang <wxzhang@126.com>
2023-04-10 19:44:37 +0800

    feat: add consts

---
+++ packages/runtime/src/consts.ts
@@ -0,0 +1,6 @@
+// 默认回退语言
+export const DefaultFallbackLanguage = 'en'                        
+export const DefaultLanguageSettings =  [
+    {name: "zh",title: "中文",default:true,active:true},
+    {name: "en",title: "英文"}
+] 
