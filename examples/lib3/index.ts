1c24aef2bf46d2bb48fd40d41584fa6721cd136d
zhangfisher <wxzhang@126.com>
2023-09-12 16:21:19 +0800

    build: 修改杂项

--- examples/lib3/index.ts
+++ examples/lib3/index.ts
@@ -1,4 +1,4 @@
-import { t } from "./languages"
+import { t } from "./i18n"
 export default {
     print(){
         console.log(t("迎接中华民族的伟大复兴！"))
--- packages/cli/extract.command.js
+++ packages/cli/extract.command.js
@@ -14,7 +14,8 @@ const { getCliLanguage } = require("./oslocate")
 function extract(srcPath,options={}){
     let { filetypes,exclude,entry} =  options
     if(!filetypes) filetypes = ["*.js","*.jsx","*.ts","*.tsx","*.vue","*.html"]
-    if(!Array.isArray(filetypes)) filetypes = filetypes.split(",")
+    if(!Array.isArray(filetypes)) filetypes = filetypes.split(",")    
+            
     const folders = filetypes.map(ftype=>{
         if(ftype.startsWith(".")) ftype = "*"+ftype
         if(!ftype.startsWith("*.")) ftype = "*."+ftype
@@ -64,8 +65,8 @@ program
     .option('-a, --activeLanguage', t('激活语言'), 'zh')  
     .option('-ns, --namespaces', t('翻译名称空间'))  
     .option('-e, --exclude <folders>', t('排除要扫描的文件夹，多个用逗号分隔'))
-    .option('-u, --updateMode', t('本次提取内容与已存在内容的数据合并策略,默认取值sync=同步,overwrite=覆盖,merge=合并'), 'sync')  
-    .option('-f, --filetypes', t('要扫描的文件类型'), 'js,vue,html,jsx,ts,tsx,mjs,cjs')  
+    .option('-u, --updateMode <value>', t('本次提取内容与已存在内容的数据合并策略,默认取值sync=同步,overwrite=覆盖,merge=合并'), 'sync')  
+    .option('-f, --filetypes <values...> ', t('要扫描的文件类型'))  
     .argument('[location]', t('工程项目所在目录'),"./")
     .hook("preAction",async function(location){ 
         await i18nScope.change(getCliLanguage())     
