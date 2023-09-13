a50b1d765bbd92d1a4aedf3036e8d538ebd7a042
zhangfisher <wxzhang@126.com>
2023-09-13 10:55:24 +0800

    update

--- packages/cli/extract.command.js
+++ packages/cli/extract.command.js
@@ -66,7 +66,7 @@ program
     .option('-ns, --namespaces', t('翻译名称空间'))  
     .option('-e, --exclude <folders>', t('排除要扫描的文件夹，多个用逗号分隔'))
     .option('-u, --updateMode <value>', t('本次提取内容与已存在内容的数据合并策略,默认取值sync=同步,overwrite=覆盖,merge=合并'), 'sync')  
-    .option('-f, --filetypes <values...> ', t('要扫描的文件类型'))  
+    .option('-f, --filetypes <values...> ', t('要扫描的文件类型'),[])  
     .argument('[location]', t('工程项目所在目录'),"./")
     .hook("preAction",async function(location){ 
         await i18nScope.change(getCliLanguage())     
