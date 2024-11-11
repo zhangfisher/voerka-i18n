0c6d5bda1a3074599904b07b303af609b34d81bb
zhangfisher <wxzhang@126.com>
2024-11-11 10:46:47 +0800

    fix: 修复@voerkai8n/vue引入@voerkai18n/runtime的拼写错误导致的构建失败

--- packages/vue/src/types.ts
+++ packages/vue/src/types.ts
@@ -1,15 +1,8 @@
-import type { VoerkaI18nScope, VoerkaI18nTranslate } from "@Voerkai18n/runtime"
+import type { VoerkaI18nScope, VoerkaI18nTranslate } from "@voerkai18n/runtime"
 
 export declare interface VoerkaI18nVuePluginOptions{
     i18nScope:VoerkaI18nScope
 }
- 
-
-// export declare type VoerkaI18nProvider = InjectionKey<VoerkaI18nProviderType>
-// export declare  function injectVoerkaI18n():VoerkaI18nProviderType
-
-//@vue/runtime-core'
-
   
 declare module 'vue' { 
     interface ComponentCustomProperties  {
