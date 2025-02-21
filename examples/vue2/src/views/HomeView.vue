51fdbe6cd6afc8a2859fca618b951eeb25a6cd94
wxzhang <wxzhang@126.com>
2025-02-21 21:03:46 +0800

    update

--- examples/vue2/src/views/HomeView.vue
+++ examples/vue2/src/views/HomeView.vue
@@ -1,19 +1,19 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld :msg="t('VoerkaI8n全流程国际化解决方案')"/>
+    <VoerkaI18n :msg="t('VoerkaI8n全流程国际化解决方案')"/>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import VoerkaI18n from '@/components/VoerkaI18n.vue'; // @ is an alias to /src
 
 
 export default Vue.extend({
   name: 'HomeView',
   components: {
-    HelloWorld,
+    VoerkaI18n,
   },
 });
 </script>
