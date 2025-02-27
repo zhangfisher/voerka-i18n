edf835372a4d4b036313f6a55994b22f2ba60181
wxzhang <wxzhang@126.com>
2025-02-27 20:48:20 +0800

    update

---
+++ packages/utils/src/getLanguageDomains.ts
@@ -0,0 +1,36 @@
+/**
+ * 
+ * 获取语言文件域Domains
+ * 
+ * 
+ */
+import { getLanguageDir } from "./getLanguageDir";
+import path from "node:path"
+import { glob } from "glob"
+
+/**
+ * 
+ * {default:"路径",}
+ * {server:"languages/server", client:"languages/client"}
+ * 
+ * @returns {Record<string,string>} 
+ * 
+ */
+export function getLanguageDomains():Record<string,string>{
+    const langDir = getLanguageDir();
+    const files = glob.sync( "*.*", {
+                            cwd:langDir, 
+                            mark:true,
+                            absolute:true
+                        })
+    if(files.some(file=>["idMap.json","settings.json"].includes(path.basename(file)))){
+        return {default:langDir}
+    }else{
+        return files.filter(file=>file.endsWith(path.sep)).reduce<Record<string,string>>((result,file)=>{
+            result[path.basename(file)] = file
+            return result 
+        },{})
+    } 
+}
+  
+  
